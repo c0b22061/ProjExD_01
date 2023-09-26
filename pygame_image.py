b5b9702738d82734d42b8b17f36becbb414c868c
taku <c0b22061a5@edu.teu.ac.jp>
2023-09-26 16:44:36 +0900

    ensyuu

--- pygame_image.py
+++ pygame_image.py
@@ -7,6 +7,7 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
+    bg_img1 =pg.transform.flip(bg_img,True,False)
     tori_img = pg.image.load("ex01/fig/3.png")
     tori_img =pg.transform.flip(tori_img,True,False)
     tori_img1=pg.transform.rotozoom(tori_img,10,1.0)
@@ -15,10 +16,15 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        a=(tmr%1600)
+        a=(tmr%2400)
+        print(a)
+        b=tmr%200
         screen.blit(bg_img, [-a, 0])
-        screen.blit(bg_img, [1600-a, 0])
-        screen.blit(tori_imgs[tmr%2],[300,200])
+        screen.blit(bg_img1, [1600-a, 0])
+        if b>100:
+             screen.blit(tori_imgs[0],[300,200])
+        else:
+            screen.blit(tori_imgs[1],[300,200])
         pg.display.update()
         tmr += 1     
         clock.tick(100)
